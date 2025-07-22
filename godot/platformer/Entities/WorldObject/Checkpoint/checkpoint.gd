c5b8e960d78816b5d0debad8d35a38c2c34249dd
Arthur <arthur.schuelter@gmail.com>
2025-07-22 13:29:26 -0300

    godot-platformer: removed

--- godot/platformer/Entities/WorldObject/Checkpoint/checkpoint.gd
+++
@@ -1,5 +0,0 @@
-extends WorldObject
-class_name Checkpoint
-
-func _on_body_entered(player: Player) -> void:
-	player.set_checkpoint(self.global_position)
--- godot/platformer/Entities/WorldObject/Spike/spike.gd
+++
@@ -1,7 +0,0 @@
-extends WorldObject
-class_name Spike
-
-func _on_body_entered(player: Player) -> void:
-	#player.check_point = floor(player.check_point / 8) * 8
-	print(player.check_point)
-	player.respawn()
